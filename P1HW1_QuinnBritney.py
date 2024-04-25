7a7961600fecd0986f32094fbff904909740684b
quinnb6283 <166692313+quinnb6283@users.noreply.github.com>
2024-04-25 02:16:12 -0400

    Add files via upload
    P1HW1_QuinnBritney

---
+++ P1HW1_QuinnBritney.py
@@ -0,0 +1,35 @@
+#Britney Quinn
+#04/11/2024
+#P1LAB1
+#To write code that collects information from user,processes information collected and display results to user.
+
+
+
+
+
+num_1 = int(input("Enter Integer:\n"))
+
+print ("You entered:", num_1)
+
+squared = num_1**2
+
+
+print (num_1,"squared is", squared)
+
+squared = num_1**3
+
+print ("And",num_1,"cubed is", num_1**3,"!!")
+
+
+num_2 = int(input("Enter another integer:\n"))
+
+
+add = num_1+num_2
+
+
+print(num_1,"+",num_2,"is", add)
+
+
+multiply = num_1 * num_2
+
+print(num_1,"*",num_2,"is", multiply)
